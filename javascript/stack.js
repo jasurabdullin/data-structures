7b82fda48810bafa9bd9ffdf86dfe25d762bb615
jasur <tarancci@gmail.com>
2021-08-16 15:59:16 -0400

    feat: created a stack data structure with JS

--- javascript/stack.js
+++ javascript/stack.js
@@ -21,9 +21,72 @@ class Stack {
         this.data.push(record)
     }
 
-    pop(record) {
-        return this.data.shift(record)
+    pop() {
+        return this.data.pop()
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
     }
 };
 
 module.exports = Stack;
+
+
+// EXERCISE
+
+// Implement a Queue datastructure using two stacks.
+// *Do not* create an array inside of the 'Queue' class.
+// Queue should implement the methods 'add', 'remove', and 'peek'.
+// For a reminder on what each method does, look back at the Queue exercise.
+
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.add(2);
+//     q.peek();  // returns 1
+//     q.remove(); // returns 1
+//     q.remove(); // returns 2
+
+class Queue {
+    constructor(){
+        this.firstStack = new Stack();
+        this.secondStack = new Stack();
+    }
+
+    add(record) {
+        this.firstStack.push(record);
+    }
+
+    remove() {
+        while (this.firstStack.peek()) {
+            const record = this.firstStack.pop()
+            this.secondStack.push(record);
+        }
+
+        const record = this.secondStack.pop();
+
+        while (this.secondStack.peek()) {
+            const record = this.secondStack.pop()
+            this.firstStack.push(record);
+        }
+
+        return record;
+    }
+
+    peek() {
+        while (this.firstStack.peek()) {
+            this.secondStack.push(this.firstStack.pop());
+        }
+
+        const record = this.secondStack.peek();
+
+        while (this.secondStack.peek()) {
+            this.firstStack.push(this.secondStack.pop());
+        }
+
+        return record;
+    }
+};
+
+module.exports = Queue;
